c2e56ca65f065ba651356f21392921f863b54004
kss <kss@ec0f27a9-8735-0410-b0c3-3338c583ac1a>
2010-04-07 18:39:31 +0000

    initial checkin (all hyuna code as downloaded from lookingglass)
    git-svn-id: https://cs-svn.jax.org/svn/csrepos/geno-vino-pipeline/trunk/dev@9973 ec0f27a9-8735-0410-b0c3-3338c583ac1a

---
+++ R/utility.R
@@ -0,0 +1,40 @@
+plotSNP = function(nm, ns, geno, vino, main='', xlab='Contrast',  ylab = 'Average Intensity'){
+  plot(nm, ns, xlab = xlab, ylab= ylab, main=main)
+  points(nm[geno==1], ns[geno == 1], col=2) 
+  points(nm[geno==2], ns[geno == 2], col=3) 
+  points(nm[geno==3], ns[geno == 3], col=4) 
+  points(nm[vino==1], ns[vino == 1], pch = 19, col=6) 
+}
+
+imageplot = function(filename, plotname = 'imageplot.jpg' ){
+  library(affyio);
+  my <- as.matrix(read.celfile(filename, intensity.means.only=TRUE)[["INTENSITY"]][["MEAN"]][1:6892960] )
+  y=matrix(log2(my), byrow=T,ncol=2680) # y[ypos+1,xpos+1] 2572 * 2680
+  jpeg(plotname, width = 1200, heigh = 1200)
+  image(c(1:2572), c(1:2680), y, col=c('green','black','red'), xlab='', ylab='' )
+  dev.off()
+}
+
+densityplot = function(filenames, plotname='densityplot.jpg'){
+  library(affyio); library('preprocessCore'); 
+
+  nfile = length(filenames)
+  jpeg(plotname, width = 1200, heigh = 1200)
+  y = as.matrix(read.celfile(as.character( filenames[1] ), intensity.means.only=TRUE)[["INTENSITY"]][["MEAN"]][fid])
+  y = log2(y)
+  allAint = y[allAid,1,drop=FALSE] 
+  allBint = y[allBid,1,drop=FALSE] 
+  allAint <- subColSummarizeMedian(matrix( allAint, ncol=1), allA)
+  allBint <- subColSummarizeMedian(matrix( allBint, ncol=1), allA)
+  plot( density( c(allAint, allBint) ) , xlab='', ylab='')
+  for(i in 2:nfile){
+    y = as.matrix(read.celfile(as.character( filenames[i] ), intensity.means.only=TRUE)[["INTENSITY"]][["MEAN"]][fid])
+    y = log2(y)
+    allAint = y[allAid,1,drop=FALSE] 
+    allBint = y[allBid,1,drop=FALSE] 
+    allAint <- subColSummarizeMedian(matrix( allAint, ncol=1), allA)
+    allBint <- subColSummarizeMedian(matrix( allBint, ncol=1), allA)
+    lines( density( c(allAint, allBint) ) )
+  }
+  dev.off()
+}
