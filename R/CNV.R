10ee63fc659128d1ae8b71b5f03897a5eb206fed
Keith Sheppard <keithshep@gmail.com>
2010-07-30 13:58:21 -0400

    reformatted a bunch of code in CNV.R to make it a bit easier to read

--- R/CNV.R
+++ R/CNV.R
@@ -1,17 +1,45 @@
-pennCNVinput <- function(chrid, mpos, exon1info, exon2info, celfiledir, 
-    filenames, snpsavefiledir, exonoutfiledir, cnvoutfiledir, mchr) {
+pennCNVinput <- function(
+    chrid,
+    mpos,
+    exon1info,
+    exon2info,
+    celfiledir,
+    filenames,
+    snpsavefiledir,
+    exonoutfiledir,
+    cnvoutfiledir,
+    mchr)
+{
     exon1CNV <- length(exon1info) > 0
     exon2CNV <- length(exon2info) > 0
     
     # normalize and save all of the exon probeset intensity info
-    if (exon1CNV) 
-        e1 <- igp(celfiledir, exonoutfiledir, outfilename = "exon1", exon1info$exon1id, 
-            exon1info$chrid, exon1info$mpos, exon1info$exon1, exon1info$CGFLcorrection, 
-            exon1info$reference, filenames, mchr)
-    if (exon2CNV) 
-        e2 <- igp(celfiledir, exonoutfiledir, outfilename = "exon2", exon2info$exon2id, 
-            exon2info$chrid, exon2info$mpos, exon2info$exon2, exon2info$CGFLcorrection, 
-            exon2info$reference, filenames, mchr)
+    if (exon1CNV)
+        e1 <- igp(
+            celfiledir,
+            exonoutfiledir,
+            outfilename = "exon1",
+            exon1info$exon1id,
+            exon1info$chrid,
+            exon1info$mpos,
+            exon1info$exon1,
+            exon1info$CGFLcorrection,
+            exon1info$reference,
+            filenames,
+            mchr)
+    if (exon2CNV)
+        e2 <- igp(
+            celfiledir,
+            exonoutfiledir,
+            outfilename = "exon2",
+            exon2info$exon2id,
+            exon2info$chrid,
+            exon2info$mpos,
+            exon2info$exon2,
+            exon2info$CGFLcorrection,
+            exon2info$reference,
+            filenames,
+            mchr)
     
     allbaf <- NULL
     alllrr <- NULL
@@ -88,11 +116,14 @@ pennCNVinput <- function(chrid, mpos, exon1info, exon2info, celfiledir,
         # save LRR and BAF for this sample (includes all SNPs and exons)
         tmp <- c(allbaf[, sampleid], rep(2, exonsize))
         s <- cbind(nn, alrr, tmp)
-        colnames(s) <- c("Name", paste(gsub("CEL", "Log R Ratio", filenames[sampleid]), 
-            sep = "", collapse = ""), paste(gsub("CEL", "B Allele Freq", filenames[sampleid]), 
-            sep = "", collapse = ""))
-        xname1 <- paste(cnvoutfiledir, "/cnv", gsub(".CEL", ".txt", filenames[sampleid]), 
-            sep = "", collapse = "")
+        colnames(s) <- c(
+            "Name",
+            paste(gsub("CEL", "Log R Ratio", filenames[sampleid]), sep = "", collapse = ""),
+            paste(gsub("CEL", "B Allele Freq", filenames[sampleid]), sep = "", collapse = ""))
+        xname1 <- paste(
+            cnvoutfiledir,
+            "/cnv",
+            gsub(".CEL", ".txt", filenames[sampleid]), sep = "", collapse = "")
         write.table(s, file = xname1, row.names = FALSE, sep = "\t", quote = FALSE)
     }
     
@@ -360,8 +391,7 @@ igp <- function(
         y <- subColSummarizeMedian(matrix(y, ncol = 1), ename)
         g <- match(chrid, mchr)
         y <- y[match(names(chrid[!is.na(g)]), rownames(y)), ]
-        xname2 <- paste(outfiledir, "/", gsub(".CEL", outfilename, filenames[i]), 
-            sep = "", collapse = "")
+        xname2 <- paste(outfiledir, "/", gsub(".CEL", outfilename, filenames[i]), sep = "", collapse = "")
         save(y, file = xname2)
         if(i == 1) 
         {
@@ -378,8 +408,17 @@ igp <- function(
     ty
 }
 
-simpleigp <- function(celfiledir, id, chrid, mpos, ename, CGFLcorrection, 
-    reference, filenames, mchr) {
+simpleigp <- function(
+    celfiledir,
+    id,
+    chrid,
+    mpos,
+    ename,
+    CGFLcorrection,
+    reference,
+    filenames,
+    mchr)
+{
     setwd(celfiledir)
     
     y <- as.matrix(read.celfile(as.character(filenames), intensity.means.only = TRUE)[["INTENSITY"]][["MEAN"]][id])
@@ -396,8 +435,21 @@ simpleigp <- function(celfiledir, id, chrid, mpos, ename, CGFLcorrection,
     out
 }
 
-simpleCNV <- function(allid, ABid, chrid, CGFLcorrection, reference, exon1info, 
-    exon2info, celfiledir, filenames, cnvoutfiledir, mchr = c(1:19), stname, refsample) {
+simpleCNV <- function(
+    allid,
+    ABid,
+    chrid,
+    CGFLcorrection,
+    reference,
+    exon1info, 
+    exon2info,
+    celfiledir,
+    filenames,
+    cnvoutfiledir,
+    mchr = c(1:19),
+    stname,
+    refsample)
+{
     if (missing(celfiledir)) 
         celfiledir <- getwd()
     if (missing(cnvoutfiledir)) 
@@ -406,13 +458,34 @@ simpleCNV <- function(allid, ABid, chrid, CGFLcorrection, reference, exon1info,
         stname <- filenames
     refid <- match(refsample, filenames)
     stname <- as.character(stname)
-    simpleCNVdata(allid, ABid, chrid, CGFLcorrection, reference, exon1info, exon2info, 
-        celfiledir, filenames, cnvoutfiledir, mchr)
+    simpleCNVdata(
+        allid,
+        ABid,
+        chrid,
+        CGFLcorrection,
+        reference,
+        exon1info,
+        exon2info,
+        celfiledir,
+        filenames,
+        cnvoutfiledir,
+        mchr)
     simpleCNVsummary(cnvoutfiledir, filenames, refid, mchr, stname)
 }
 
-simpleCNVdata <- function(allid, ABid, chrid, CGFLcorrection, reference, 
-    exon1info, exon2info, celfiledir, filenames, cnvoutfiledir, mchr) {
+simpleCNVdata <- function(
+    allid,
+    ABid,
+    chrid,
+    CGFLcorrection,
+    reference,
+    exon1info,
+    exon2info,
+    celfiledir,
+    filenames,
+    cnvoutfiledir,
+    mchr)
+{
     setwd(celfiledir)
     SNPname <- ABid$SNPname
     Aid <- ABid$allAid
@@ -439,23 +512,38 @@ simpleCNVdata <- function(allid, ABid, chrid, CGFLcorrection, reference,
         B <- normalize.quantiles.use.target(B, target = exon1info$reference)
         B <- subColSummarizeMedian(matrix(B, ncol = 1), SNPname)
         
-        exon1 <- simpleigp(celfiledir, exon1info$exon1id, exon1info$chrid, exon1info$mpos, 
-            exon1info$exon1, exon1info$CGFLcorrection, exon1info$reference, filenames[i], 
+        exon1 <- simpleigp(
+            celfiledir,
+            exon1info$exon1id,
+            exon1info$chrid,
+            exon1info$mpos,
+            exon1info$exon1,
+            exon1info$CGFLcorrection,
+            exon1info$reference,
+            filenames[i],
             mchr)
-        exon2 <- simpleigp(celfiledir, exon2info$exon2id, exon2info$chrid, exon2info$mpos, 
-            exon2info$exon2, exon2info$CGFLcorrection, exon2info$reference, filenames[i], 
+        exon2 <- simpleigp(
+            celfiledir,
+            exon2info$exon2id,
+            exon2info$chrid,
+            exon2info$mpos,
+            exon2info$exon2,
+            exon2info$CGFLcorrection,
+            exon2info$reference,
+            filenames[i],
             mchr)
         for (chri in mchr) {
             pos <- c(mpos[chrid == chri], exon1[[chri]]$pos, exon2[[chri]]$pos)
-            id <- c(rep(1, sum(chrid == chri)), rep(2, length(exon1[[chri]]$pos)), 
+            id <- c(
+                rep(1, sum(chrid == chri)),
+                rep(2, length(exon1[[chri]]$pos)),
                 rep(3, length(exon2[[chri]]$pos)))
             inte <- c(B[chrid == chri], exon1[[chri]]$ey, exon2[[chri]]$ey)
             o <- order(pos)
             pos <- pos[o]
             id <- id[o]
             inte <- inte[o]
-            xname2 <- paste(cnvoutfiledir, "/", gsub(".CEL", "Chr", filenames[i]), 
-                chri, sep = "", collapse = "")
+            xname2 <- paste(cnvoutfiledir, "/", gsub(".CEL", "Chr", filenames[i]), chri, sep = "", collapse = "")
             save(inte, file = xname2)
             if (i == 1) {
                 xname2 <- paste(cnvoutfiledir, "/pos", chri, sep = "", collapse = "")
@@ -469,27 +557,47 @@ simpleCNVsummary <- function(cnvoutfiledir, filenames, refid, mchr, stname) {
     # TODO add HiddenMarkov as a suggested library to NAMESPACE
     library(HiddenMarkov)
     k <- 0.9999
-    pi <- matrix(c(k, (1 - k)/2, (1 - k)/2, (1 - k)/2, k, (1 - k)/2, (1 - k)/2, (1 - 
-        k)/2, k), 3, 3)
+    pi <- matrix(
+        c(
+            k,
+            (1 - k)/2,
+            (1 - k)/2,
+            (1 - k)/2,
+            k,
+            (1 - k)/2,
+            (1 - k)/2,
+            (1 - k)/2,
+            k),
+        3,
+        3)
     delta <- c(0, 1, 0)
     nfile <- length(filenames)
     for (chri in mchr) {
         xname2 <- paste(cnvoutfiledir, "/pos", chri, sep = "", collapse = "")
         load(xname2)
-        xname2 <- paste(cnvoutfiledir, "/", gsub(".CEL", "Chr", filenames[refid]), 
-            chri, sep = "", collapse = "")
+        xname2 <- paste(
+            cnvoutfiledir,
+            "/",
+            gsub(".CEL", "Chr", filenames[refid]),
+            chri,
+            sep = "",
+            collapse = "")
         load(xname2)
         ref <- inte
         cnv <- NULL
         cnvtable <- NULL
         for (i in 1:nfile) {
-            xname2 <- paste(cnvoutfiledir, "/", gsub(".CEL", "Chr", filenames[i]), 
-                chri, sep = "", collapse = "")
+            xname2 <- paste(
+                cnvoutfiledir,
+                "/",
+                gsub(".CEL", "Chr", filenames[i]),
+                chri,
+                sep = "",
+                collapse = "")
             load(xname2)
             a <- inte/ref
             m <- mean(a)
-            b <- dthmm(a, pi, delta, "norm", list(mean = c(m - 0.1, m, m + 0.1), sd = c(0.05, 
-                0.05, 0.05)))
+            b <- dthmm(a, pi, delta, "norm", list(mean = c(m - 0.1, m, m + 0.1), sd = c(0.05, 0.05, 0.05)))
             states <- Viterbi(b)
             cnv <- cbind(cnv, states)
             
@@ -504,18 +612,47 @@ simpleCNVsummary <- function(cnvoutfiledir, filenames, refid, mchr, stname) {
                 ep <- c(ep, lg/2)
                 s <- length(sp)
                 if (s == 1) 
-                  cnvtable <- rbind(cnvtable, c(stname[i], "gain", g[sp, 2], g[ep, 
-                    2], g[ep, ] - g[sp, ] + 1, sum(id[g[sp, 1]:g[ep, 1]] == 1), sum(id[g[sp, 
-                    1]:g[ep, 1]] == 2), sum(id[g[sp, 1]:g[ep, 1]] == 3), round(mean(ref[g[sp, 
-                    1]:g[ep, 1]]), 3), round(mean(inte[g[sp, 1]:g[ep, 1]]), 3)))
+                  cnvtable <- rbind(
+                      cnvtable,
+                      c(
+                          stname[i],
+                          "gain",
+                          g[sp, 2],
+                          g[ep, 2],
+                          g[ep, ] - g[sp, ] + 1,
+                          sum(id[g[sp, 1]:g[ep, 1]] == 1),
+                          sum(id[g[sp, 1]:g[ep, 1]] == 2),
+                          sum(id[g[sp, 1]:g[ep, 1]] == 3),
+                          round(mean(ref[g[sp, 1]:g[ep, 1]]), 3),
+                          round(mean(inte[g[sp, 1]:g[ep, 1]]), 3)))
                 if (s > 1) {
-                  k <- cbind(g[sp, 2], g[ep, 2], g[ep, ] - g[sp, ] + 1, g[sp, 1], 
-                    g[ep, 1])
-                  k1 <- apply(k, 1, function(x, ref, id, inte) c(sum(id[x[6]:x[5]] == 
-                    1), sum(id[x[6]:x[5]] == 2), sum(id[x[6]:x[5]] == 3), mean(ref[x[6]:x[5]]), 
-                    mean(inte[x[6]:x[5]])), ref, id, inte)
-                  cnvtable <- rbind(cnvtable, cbind(rep(stname[i], s), rep("gain", 
-                    s), k[, 1:4], t(round(k1, 3))))
+                  k <- cbind(
+                      g[sp, 2],
+                      g[ep, 2],
+                      g[ep, ] - g[sp, ] + 1,
+                      g[sp, 1],
+                      g[ep, 1])
+                  k1 <- apply(
+                      k,
+                      1,
+                      function(x, ref, id, inte) {
+                          c(
+                              sum(id[x[6]:x[5]] == 1),
+                              sum(id[x[6]:x[5]] == 2),
+                              sum(id[x[6]:x[5]] == 3),
+                              mean(ref[x[6]:x[5]]),
+                              mean(inte[x[6]:x[5]]))
+                      },
+                      ref,
+                      id,
+                      inte)
+                  cnvtable <- rbind(
+                      cnvtable,
+                      cbind(
+                          rep(stname[i], s),
+                          rep("gain", s),
+                          k[, 1:4],
+                          t(round(k1, 3))))
                 }
             }
             if (ll > 2) {
@@ -525,24 +662,57 @@ simpleCNVsummary <- function(cnvoutfiledir, filenames, refid, mchr, stname) {
                 ep <- c(ep, ll/2)
                 s <- length(sp)
                 if (s == 1) 
-                  cnvtable <- rbind(cnvtable, c(stname[i], "loss", l[sp, 2], l[ep, 
-                    2], l[ep, ] - l[sp, ] + 1, sum(id[l[sp, 1]:l[ep, 1]] == 1), sum(id[l[sp, 
-                    1]:l[ep, 1]] == 2), sum(id[l[sp, 1]:l[ep, 1]] == 3), round(mean(ref[l[sp, 
-                    1]:l[ep, 1]]), 3), round(mean(inte[l[sp, 1]:l[ep, 1]]), 3)))
+                  cnvtable <- rbind(
+                      cnvtable,
+                      c(
+                          stname[i],
+                          "loss",
+                          l[sp, 2],
+                          l[ep, 2],
+                          l[ep, ] - l[sp, ] + 1,
+                          sum(id[l[sp, 1]:l[ep, 1]] == 1),
+                          sum(id[l[sp, 1]:l[ep, 1]] == 2),
+                          sum(id[l[sp, 1]:l[ep, 1]] == 3),
+                          round(mean(ref[l[sp, 1]:l[ep, 1]]), 3),
+                          round(mean(inte[l[sp, 1]:l[ep, 1]]), 3)))
                 if (s > 1) {
-                  k <- cbind(l[sp, 2], l[ep, 2], l[ep, ] - l[sp, ] + 1, l[sp, 1], 
-                    l[ep, 1])
-                  k1 <- apply(k, 1, function(x, ref, id, inte) c(sum(id[x[6]:x[5]] == 
-                    1), sum(id[x[6]:x[5]] == 2), sum(id[x[6]:x[5]] == 3), mean(ref[x[6]:x[5]]), 
-                    mean(inte[x[6]:x[5]])), ref, id, inte)
-                  cnvtable <- rbind(cnvtable, cbind(rep(stname[i], s), rep("loss", 
-                    s), k[, 1:4], t(round(k1, 3))))
+                  k <- cbind(l[sp, 2], l[ep, 2], l[ep, ] - l[sp, ] + 1, l[sp, 1], l[ep, 1])
+                  k1 <- apply(
+                      k,
+                      1,
+                      function(x, ref, id, inte) {
+                          c(
+                              sum(id[x[6]:x[5]] == 1),
+                              sum(id[x[6]:x[5]] == 2),
+                              sum(id[x[6]:x[5]] == 3),
+                              mean(ref[x[6]:x[5]]),
+                              mean(inte[x[6]:x[5]]))
+                      },
+                      ref,
+                      id,
+                      inte)
+                  cnvtable <- rbind(
+                      cnvtable,
+                      cbind(
+                          rep(stname[i], s),
+                          rep("loss", s),
+                          k[, 1:4],
+                          t(round(k1, 3))))
                 }
             }
         }
-        colnames(cnvtable) <- c("Name", "Status", "StartPosition", "EndPosition", 
-            "Number of Probe sets", "Size", "Number of SNP probe sets", "Number of exon1 sets", 
-            "Number of exon2 sets", "Mean intensity of reference sample", "Mean intensity of sample")
+        colnames(cnvtable) <- c(
+            "Name",
+            "Status",
+            "StartPosition",
+            "EndPosition",
+            "Number of Probe sets",
+            "Size",
+            "Number of SNP probe sets",
+            "Number of exon1 sets",
+            "Number of exon2 sets",
+            "Mean intensity of reference sample",
+            "Mean intensity of sample")
         colnames(cnv) <- stname
         xname <- paste("~/cnvChr", chri, sep = "", collapse = "")
         save(cnv, cnvtable, file = xname)
