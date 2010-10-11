13187ebbbd4deb0c507553de030ad00e155f87d7
Keith Sheppard <keithshep@gmail.com>
2010-10-11 15:56:24 -0400

    updated plot functions to make sure they work with any R rendering device and they work with updated metadata structure

--- R/utility.R
+++ R/utility.R
@@ -6,56 +6,65 @@ plotSNP <- function(nm, ns, geno, vino, main = "", xlab = "Contrast", ylab = "Av
     points(nm[vino == 1], ns[vino == 1], pch = 19, col = 6) # filled point
 }
 
-imageplot <- function(filename, plotname = "imageplot.jpg") {
-    my <- as.matrix(read.celfile(filename, intensity.means.only = TRUE)[["INTENSITY"]][["MEAN"]][1:6892960])
-    # y[ypos+1,xpos+1] 2572 * 2680
-    y <- matrix(log2(my), byrow = T, ncol = 2680)
-    jpeg(plotname, width = 1200, heigh = 1200)
-    image(c(1:2572), c(1:2680), y, col = c("green", "black", "red"), xlab = "", ylab = "")
-    dev.off()
+plotMouseDivArrayImage <- function(celFilename)
+{
+    my <- as.matrix(read.celfile(celFilename, intensity.means.only = TRUE)[["INTENSITY"]][["MEAN"]][1:6892960])
+    image(
+        1 : 2572,
+        1 : 2680,
+        matrix(log2(my), byrow = T, ncol = 2680),
+        col = c("green", "black", "red"),
+        xlab = "",
+        ylab = "")
 }
 
-densityplot <- function(filenames, allid, ABid, type = c("Average", "MatchedSet"), 
-    plotname = "densityplot.jpg") {
-    SNPname <- ABid$SNPname
-    Aid <- ABid$allAid
-    Bid <- ABid$allBid
-    rm(ABid)
-    nfile <- length(filenames)
-    type2 <- type == "MatchedSet"
-    jpeg(plotname, width = 1200, heigh = 1200)
-    y <- as.matrix(read.celfile(as.character(filenames[1]), intensity.means.only = TRUE)[["INTENSITY"]][["MEAN"]][allid])
-    y <- log2(y)
-    allAint <- y[Aid, 1, drop = FALSE]
-    allBint <- y[Bid, 1, drop = FALSE]
-    allAint <- subColSummarizeMedian(matrix(allAint, ncol = 1), SNPname)
-    allBint <- subColSummarizeMedian(matrix(allBint, ncol = 1), SNPname)
-    if (type2) {
-        xlab <- "Matched sequence intensity"
-        tmp <- allAint
-        tmp[allAint < allBint] <- allBint[allAint < allBint]
-    }
-    else {
-        xlab <- "Average intensity"
-        tmp <- (allAint + allBint)/2
-    }
-    plot(density(tmp), xlab = xlab, ylab = "")
-    for (i in 2:nfile) {
-        y <- as.matrix(read.celfile(as.character(filenames[i]), intensity.means.only = TRUE)[["INTENSITY"]][["MEAN"]][allid])
+mouseDivDensityPlot <- function(celFilenames, snpProbeInfo, type = c("Average", "MatchedSet"))
+{
+    type <- match.arg(type)
+    isMatchedSet <- type == "MatchedSet"
+    
+    for(i in 1 : length(celFilenames))
+    {
+        y <- as.matrix(read.celfile(as.character(celFilenames[i]), intensity.means.only = TRUE)[["INTENSITY"]][["MEAN"]][snpProbeInfo$probeIndex])
         y <- log2(y)
-        allAint <- y[Aid, 1, drop = FALSE]
-        allBint <- y[Bid, 1, drop = FALSE]
-        allAint <- subColSummarizeMedian(matrix(allAint, ncol = 1), SNPname)
-        allBint <- subColSummarizeMedian(matrix(allBint, ncol = 1), SNPname)
-        if (type2) {
+        allAint <- y[snpProbeInfo$isAAllele, 1, drop = FALSE]
+        allBint <- y[!snpProbeInfo$isAAllele, 1, drop = FALSE]
+        allAint <- subColSummarizeMedian(
+            matrix(allAint, ncol = 1),
+            snpProbeInfo$snpId[snpProbeInfo$isAAllele])
+        allBint <- subColSummarizeMedian(
+            matrix(allBint, ncol = 1),
+            snpProbeInfo$snpId[!snpProbeInfo$isAAllele])
+        
+        if(isMatchedSet)
+        {
             tmp <- allAint
             tmp[allAint < allBint] <- allBint[allAint < allBint]
         }
-        else tmp <- (allAint + allBint)/2
-        lines(density(tmp))
+        else
+        {
+            tmp <- (allAint + allBint) / 2
+        }
+        
+        if(i == 1)
+        {
+            if(isMatchedSet)
+            {
+                xlab <- "Matched sequence intensity"
+            }
+            else
+            {
+                xlab <- "Average intensity"
+            }
+            
+            plot(density(tmp), xlab = xlab, ylab = "", main = "")
+        }
+        else
+        {
+            lines(density(tmp))
+        }
     }
-    dev.off()
-} 
+}
 
 createAppendResultsToCSVFunction <- function(fileName)
 {
